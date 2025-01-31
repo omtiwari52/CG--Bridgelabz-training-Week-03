72e62c7e7954976af7da512eafb2a8dee6bfa9ac
omtiwari52 <tiwariom900@gmail.com>
2025-01-31 16:47:05 +0530

    Day03 Done

---
+++ Day03/src/QuickSort/Product.java
@@ -0,0 +1,35 @@
+package QuickSort;
+
+public class Product {
+    public void quickSort(int[] productPrices, int startIdx, int endIdx){
+        if(startIdx >= endIdx){
+            return;
+        }
+        int pivotIdx = partion(productPrices, startIdx, endIdx);
+        quickSort(productPrices, startIdx, pivotIdx-1);
+        quickSort(productPrices, pivotIdx+1, endIdx);
+    }
+
+    public int partion(int[] productPrices, int startIdx, int endIdx){
+        int pivot = productPrices[endIdx];
+
+        int i = startIdx -1;
+        for(int j = startIdx; j < endIdx; j++){
+            if(productPrices[j] <= pivot){
+                i++;
+
+                //swap
+                int temp = productPrices[j];
+                productPrices[j] = productPrices[i];
+                productPrices[i] = temp;
+            }
+        }
+        i++;
+
+        //swap
+        int temp = pivot;
+        productPrices[endIdx] = productPrices[i];
+        productPrices[i] = temp;
+        return i;
+    }
+}
