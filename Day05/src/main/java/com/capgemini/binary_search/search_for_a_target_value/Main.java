dbd02602b82ffc98c1fa3d344bcfae702ea4830f
omtiwari52 <tiwariom900@gmail.com>
2025-02-05 16:55:12 +0530

    [Om Tiwari] Add. search for target value code added✨

---
+++ Day05/src/main/java/com/capgemini/binary_search/search_for_a_target_value/Main.java
@@ -0,0 +1,39 @@
+package com.capgemini.binary_search.search_for_a_target_value;
+
+import com.capgemini.binary_search.find_the_rotation_point.FindRotationPoint;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+
+            //get user input for the size of array
+            System.out.print("Enter the size of array: ");
+            int size = Integer.parseInt(br.readLine());
+
+            int[][] arr = new int[size][size];
+            System.out.println("Enter array elements: ");
+            try {
+                for(int i = 0; i < size; i++){
+                    for (int j = 0; j < size; j++) {
+                        arr[i][j] = Integer.parseInt(br.readLine());
+                    }
+                }
+            }
+            catch(NumberFormatException e){
+                System.out.println("Exception occurred NumberFormatException found"+ e.getMessage());
+            }
+
+            System.out.print("Enter your target Value: ");
+            int target = Integer.parseInt(br.readLine());
+
+            System.out.println(SearchTargetValue.searchForTargetValue(arr,target));;
+        }
+        catch (IOException e){
+            System.out.println("Exception occurred IO Exception found"+ e.getMessage());
+        }
+    }
+}
---
+++ Day05/src/main/java/com/capgemini/binary_search/search_for_a_target_value/SearchTargetValue.java
@@ -0,0 +1,36 @@
+package com.capgemini.binary_search.search_for_a_target_value;
+
+public class SearchTargetValue {
+    public static boolean searchForTargetValue(int[][] arr, int target){
+        int row = arr.length;
+        int col = arr[0].length;
+
+        int left = 0;
+        int right = row-1;
+        int mid;
+
+        while(left <= right){
+
+            mid = left + (right - left)/2;
+
+            if(arr[mid][0] == target) return true;
+
+            else if(arr[mid][0] > target) right = mid - 1;
+            left = mid + 1;
+        }
+
+        if(right < 0) return false;
+
+        left = 0;
+        row = right;
+        right = col -1;
+        while(left <= right){
+            mid = left + (right - left)/2;
+            if(arr[row][mid] == target) return true;
+            else if(arr[row][mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return false;
+    }
+}
