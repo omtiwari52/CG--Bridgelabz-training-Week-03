3a388aa527b3ac2a64c7953512a9c39c2052f3c7
omtiwari52 <tiwariom900@gmail.com>
2025-02-08 15:17:00 +0530

    [Om Tiwari] refactor: Search for a target in a large dataset code refactored📦

--- Day06/src/main/java/com/capgemini/search_a_target_in_a_large_dataset/RandomArrayElementGeneration.java
+++ Day06/src/main/java/com/capgemini/search_a_target_in_a_large_dataset/RandomArrayElementGeneration.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomArrayElementGeneration {
 
     Random rand = new Random();
-    RandomArrayElementGeneration(){}
+    public RandomArrayElementGeneration(){}
 
     public void generateRandomElement(int[] arr, int N){
         for(int i = 0; i < arr.length; i++){
